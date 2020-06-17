64828957a4e3bcb7c605ffe5e9649576fec90789
Robert Pham <marwinnortington@gmail.com>
2020-06-17 17:47:12 +0700

    Setting up screen 3

---
+++ src/components/styles/container.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+export const global_styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
---
+++ src/constants/styles/padding.js
@@ -0,0 +1,2 @@
+export const HEADER_TITLE_PADDING_TOP = 20;
+export const HEADER_TITLE_PADDING_LEFT = 20;
