5a90cf9bd62459af23c1ed9b1fbdc8dad8e3e956
unknown <marwinnortington@gmail.com>
2020-07-28 11:53:14 +0700

    Repost full code pase

--- src/screens/core/user_joining/ProjectFilter.js
+++ src/screens/core/user_joining/ProjectFilter.js
@@ -6,73 +6,181 @@
 }
 * Contributors: {
     Hiển Long: 17 June
+     Tiến: 25 July
 }
 */
 
 // {useState} is to control the state, whether it pop up or not. Modal is to create the pop-up.
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-    Text,
-    View,
-    StyleSheet,
-    Button,
-    Modal,
-    TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Modal,
+  TouchableOpacity,
+  SafeAreaView,
+  Image
 } from 'react-native';
 
-const  ProjectFilter = (props) => {
-
-    //This is to control whether the pop-up is open or not. Not sure how this even works, but it does anyway.
-    const [modalOpen, setModalOpen] = useState(false);
-
-    return (
-        <View style={styles.container}>
-
-            <Text style={styles.paragraph}>
-                Screen 2: Choosing places, popularities and progresses.
-        </Text>
-            <TouchableOpacity>
-                <Button
-                    //Will create the pop up when pressed
-                    onPress={() => setModalOpen(true)}
-                    title="Places"
-                    color="#841584"
-                />
-                <Button color="#841584" title="Tìm dự án" onPress={() => props.navigation.navigate('ProjectList')} />
-            </TouchableOpacity>
-
-            <Modal visible={modalOpen}>
-                <View style={styles.container}>
-                    <Text> Ho Chi Minh city </Text>
-                    <Text> Da Nang </Text>
-                    <Text> Ha Noi </Text>
-                    <Button
-                        //This is for closing the Modal
-                        onPress={() => setModalOpen(false)}
-                        title="Close this shit"
-                        color="#841584"
-                    />
-                </View>
-            </Modal>
-
+const ProjectFilter = props => {
+  //This is to control whether the pop-up is open or not. Not sure how this even works, but it does anyway.
+  const [modal1Open, setModal1Open] = useState(false);
+  const [modal2Open, setModal2Open] = useState(false);
+  const [modal3Open, setModal3Open] = useState(false);
+  return (
+    /*
+    SafeAreaView covers screen2 and have a view covers 4 small others.
+    The ImageBackGround is covered in the big View.
+    View 1: Button is named "Địa Điểm"
+    View 2: Button is named "Độ Phổ Biến"
+    View 3: Button is named "Quá Trình"
+    View 4: Button is named "Tìm dự án"
+    */
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('../../../assets/images/Screen2.png')}/>
+        <View style={styles.title_1}>
+          <Button
+            color="blue"
+            title="Địa Điểm"
+            onPress={() => setModal1Open(true)}
+          />
         </View>
-    );
+
+        <View style={styles.title_2}>
+          <Button
+            color="blue"
+            title="Độ Phổ Biến"
+            onPress={() => setModal2Open(true)}
+          />
+        </View>
+
+        <View style={styles.title_3}>
+          <Button
+            color="blue"
+            title="Quá Trình"
+            onPress={() => setModal3Open(true)}
+          />
+        </View>
+
+        <View style={styles.title_4}>
+          <Button
+            color="red"
+            title="Tìm dự án"
+            onPress={() => props.navigation.navigate('ProjectList')}
+          />
+        </View>
+
+        <Modal visible={modal1Open}>
+        <View style={styles.container}>
+          <TouchableOpacity>
+          <Text style={styles.paragraph}> Ho Chi Minh city </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.paragraph}> Da Nang </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+          <Text style={styles.paragraph}> Ha Noi </Text>
+          </TouchableOpacity>
+          
+          <Button
+            //This is for closing the Modal
+            onPress={() => setModal1Open(false)}
+            title="Close this shit"
+            color="dodgerblue"
+          />
+        </View>
+      </Modal>
+
+      <Modal visible={modal2Open}>
+        <View style={styles.container}>
+          <TouchableOpacity>
+            <Text style={styles.paragraph}> Hiện tại có 100 người cách li </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.paragraph}> Da Nang cách li 200 người </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.paragraph}> Ha Noi giãn cách xã hội </Text>
+          </TouchableOpacity>
+          <Button
+            //This is for closing the Modal
+            onPress={() => setModal2Open(false)}
+            title="Close this shit"
+            color="dodgerblue"
+          />
+        </View>
+      </Modal>
+
+      <Modal visible={modal3Open}>
+        <View style={styles.container}>
+          <TouchableOpacity>
+          <Text style={styles.paragraph}> 14 ngày cách li </Text>
+          </TouchableOpacity>
+         <TouchableOpacity>
+         <Text style={styles.paragraph}> chiều 13h ngày 26/7 cách li xã hội </Text>
+         </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.paragraph}> Đà Nẵng thất thủ </Text>
+          </TouchableOpacity>
+          <Button
+            //This is for closing the Modal
+            onPress={() => setModal3Open(false)}
+            title="Close this shit"
+            color="dodgerblue"
+          />
+        </View>
+      </Modal>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center',
-        backgroundColor: '#ecf0f1',
-        padding: 12,
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+    marginBottom:12
+  },
+
+  image: {
+    resizeMode: 'contain',
+    height: 320,
+    width: 400,
+  },
+
+  title_1: {
+    textAlign: 'center',
+    marginVertical: 50,
+    width: 150,
+  },
+  title_2: {
+    textAlign: 'center',
+    marginVertical: 30,
+    width: 150,
+  },
+  title_3: {
+    textAlign: 'center',
+    marginVertical: 30,
+    width: 150,
+  },
+  title_4: {
+    textAlign: 'center',
+    marginVertical: 40,
+    width: 150,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    color:'dodgerblue',
+    fontWeight:'bold',
+    textAlign:'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
 });
 
 export default ProjectFilter;
